142dc63cede1896c8c3deede0975dbcf905dc822
Marco Caceffo <ultimatemal2k@gmail.com>
2024-05-01 22:42:08 +0200

    update

--- lua/kita/plugins/alpha.lua
+++ lua/kita/plugins/alpha.lua
@@ -79,7 +79,7 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", " > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", " > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("SPC oo", " > Toggle oil", "<cmd>Oil --float<CR>"),
 			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fs", " > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
