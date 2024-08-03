71d1a87feee26900ebe88e616a8e5937dc63fda3
Marco Caceffo <ultimatemal2k@gmail.com>
2024-08-03 12:59:17 +0200

    update

--- lua/kita/core/init.lua
+++ lua/kita/core/init.lua
@@ -14,18 +14,37 @@ if vim.g.neovide then
 end
 
 -- terminal background color fix
+-- vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+-- 	callback = function()
+-- 		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+-- 		if not normal.bg then
+-- 			return
+-- 		end
+-- 		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+-- 		io.write(string.format("\027Ptmux;\027\027]11;#%06x\007\027\\", normal.bg))
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("UILeave", {
+-- 	callback = function()
+-- 		io.write("\027]111\027\\")
+-- 		io.write("\027Ptmux;\027\027]111;\007\027\\")
+-- 	end,
+-- })
 vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
 	callback = function()
 		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
 		if not normal.bg then
 			return
 		end
+		io.write(string.format("\027Ptmux;\027\027]11;#%06x\007\027\\", normal.bg))
 		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
 	end,
 })
 
 vim.api.nvim_create_autocmd("UILeave", {
 	callback = function()
+		io.write("\027Ptmux;\027\027]111;\007\027\\")
 		io.write("\027]111\027\\")
 	end,
 })
