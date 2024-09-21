33830dbb528e26e215dd7f2fc13bee9091dab615
Marco Caceffo <ultimatemal2k@gmail.com>
2024-09-21 12:45:09 +0200

    nvim notify is now static

--- lua/kita/plugins/notify.lua
+++ lua/kita/plugins/notify.lua
@@ -1,3 +1,11 @@
 return {
 	"rcarriga/nvim-notify",
+	config = function()
+		local notify = require("notify")
+		notify.setup({
+			render = "minimal",
+			stages = "static",
+			timeout = 1000,
+		})
+	end,
 }
