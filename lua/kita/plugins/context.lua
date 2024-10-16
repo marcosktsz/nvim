33d9e80d7c173a503e36e373e12177694d08965d
Marco Caceffo <ultimatemal2k@gmail.com>
2024-10-16 20:20:20 +0200

    fixed context multiline thing and removed timeout for which-key

--- lua/kita/plugins/context.lua
+++ lua/kita/plugins/context.lua
@@ -2,5 +2,8 @@ return {
 	"nvim-treesitter/nvim-treesitter-context",
 	config = function()
 		vim.keymap.set("n", "<leader>ctx", "<cmd>TSContextToggle<CR>")
+		local treeContext = require("treesitter-context").setup({
+			multiline_threshold = 1,
+		})
 	end,
 }
