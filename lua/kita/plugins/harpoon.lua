e0f96615091272e4f47f6bc508278c9d0cae0d07
Marco Caceffo <ultimatemal2k@gmail.com>
2024-11-06 21:11:48 +0100

    Added harpoon v split

--- lua/kita/plugins/harpoon.lua
+++ lua/kita/plugins/harpoon.lua
@@ -6,6 +6,21 @@ return {
 
 	config = function()
 		local harpoon = require("harpoon")
+		harpoon:extend({
+			UI_CREATE = function(cx)
+				vim.keymap.set("n", "<C-v>", function()
+					harpoon.ui:select_menu_item({ vsplit = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-x>", function()
+					harpoon.ui:select_menu_item({ split = true })
+				end, { buffer = cx.bufnr })
+
+				vim.keymap.set("n", "<C-t>", function()
+					harpoon.ui:select_menu_item({ tabedit = true })
+				end, { buffer = cx.bufnr })
+			end,
+		})
 
 		-- REQUIRED
 		harpoon:setup()
