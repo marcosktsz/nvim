9816f954d0ed61f1f306d599d42af2279538a2c3
Marco Caceffo <marco.caceffo@solvhealth.com>
2024-11-09 22:36:13 +0100

    cute lualine lol

--- lua/kita/plugins/lualine.lua
+++ lua/kita/plugins/lualine.lua
@@ -22,10 +22,11 @@ return {
 
 		lualine.setup({
 			options = {
-				section_separators = { left = " ", right = " " },
-				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
 			},
 			sections = {
+            lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
 				lualine_y = {
 					{
 						"harpoon2",
@@ -64,6 +65,9 @@ return {
 					{ "fileformat" },
 					{ "filetype" },
 				},
+        lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
 			},
 		})
 	end,
