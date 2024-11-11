187c69611ce006c3b32fd5aecf3569ac89f810fe
Marco Caceffo <ultimatemal2k@gmail.com>
2024-11-11 20:11:32 +0100

    Update to highlight.lua

--- lua/kita/plugins/highlight.lua
+++ lua/kita/plugins/highlight.lua
@@ -1,6 +1,8 @@
 return {
 	"brenoprata10/nvim-highlight-colors",
 	config = function()
-		require("nvim-highlight-colors").setup({})
+		require("nvim-highlight-colors").setup({
+      render = 'foreground'
+    })
 	end,
 }
