d8443793e2fbda1d2fdec32d70b0a3cd8cede629
Marco Caceffo <marco.caceffo@solvhealth.com>
2024-11-21 22:51:36 +0100

    edits on codeium + cmp

--- lua/kita/plugins/codeium.lua
+++ lua/kita/plugins/codeium.lua
@@ -31,7 +31,7 @@ return {
 				-- Key bindings for managing completions in virtual text mode.
 				key_bindings = {
 					-- Accept the current completion.
-					accept = "<Tab>",
+					accept = "<C-f>",
 					-- Accept the next word.
 					accept_word = false,
 					-- Accept the next line.
--- lua/kita/plugins/nvim-cmp.lua
+++ lua/kita/plugins/nvim-cmp.lua
@@ -38,9 +38,9 @@ return {
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				-- ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				-- ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-S-Space>"] = cmp.mapping.complete(), -- show completion suggestions
