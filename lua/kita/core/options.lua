db7479b41e01822b9ad907b3b20c92ef21f4bd38
Marco Caceffo <marco.caceffo@solvhealth.com>
2024-11-30 19:00:55 +0100

    Revert "Trying to differenciate from vim and system clipboard"
    This reverts commit 08f829f01fa264c890d86e020c8774456f0f5add.

--- lua/kita/core/options.lua
+++ lua/kita/core/options.lua
@@ -34,10 +34,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
--- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
-vim.opt.clipboard = "unnamedplus"
-vim.keymap.set("n", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
-vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
