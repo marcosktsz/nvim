1c8f64dd8a80d5040676165015b0b474ce34cc86
Marco Caceffo <marco.caceffo@solvhealth.com>
2024-12-12 23:57:48 +0100

    ctrl-s as cmp suggestion

--- lua/kita/plugins/nvim-cmp.lua
+++ lua/kita/plugins/nvim-cmp.lua
@@ -43,7 +43,7 @@ return {
 				["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-S-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-s>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
