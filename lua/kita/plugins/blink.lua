292037160210f607153951ad523c47bc93c364dc
Marco Caceffo <ultimatemal2k@gmail.com>
2025-01-07 22:48:53 +0100

    removed blink comments + added tab to jump between snippets

--- lua/kita/plugins/blink.lua
+++ lua/kita/plugins/blink.lua
@@ -1,28 +1,19 @@
 return {
 	{
 		"saghen/blink.compat",
-		-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
 		version = "*",
-		-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
 		lazy = true,
-		-- make sure to set opts so that lazy.nvim calls blink.compat's setup
 		opts = {},
 	},
 	{
 		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
 		dependencies = "rafamadriz/friendly-snippets",
 
-		-- use a release tag to download pre-built binaries
 		version = "0.9.0",
 
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
 			keymap = {
 				preset = "none",
 				["<C-j>"] = {
@@ -37,15 +28,12 @@ return {
 				},
 				["<C-s>"] = { "show" },
 				["<CR>"] = { "select_and_accept", "fallback" },
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
 			},
 
 			appearance = {
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- Will be removed in a future release
 				use_nvim_cmp_as_default = true,
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
 			},
 
@@ -62,8 +50,6 @@ return {
 				},
 			},
 
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
 				default = { "lsp", "path", "snippets", "buffer", "dadbod" },
 				providers = {
