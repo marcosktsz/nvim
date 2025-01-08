4c919ba955d4343771fb66e35f502f50173c95ff
Marco Caceffo <ultimatemal2k@gmail.com>
2025-01-08 21:19:06 +0100

    moved options up to opts

--- lua/kita/plugins/noice.lua
+++ lua/kita/plugins/noice.lua
@@ -1,9 +1,6 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
@@ -12,7 +9,7 @@ return {
 		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
-	require("noice").setup({
+	opts = {
 		lsp = {
 			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 			override = {
@@ -21,7 +18,6 @@ return {
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
 		},
-		-- you can enable a preset for easier configuration
 		presets = {
 			bottom_search = false, -- use a classic bottom cmdline for search
 			command_palette = true, -- position the cmdline and popupmenu together
@@ -29,5 +25,5 @@ return {
 			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
-	}),
+	},
 }
