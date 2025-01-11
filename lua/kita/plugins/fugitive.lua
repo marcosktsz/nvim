452c4d7ae4e56abf1c30d4a76c897b4d04f7dda7
Marco Caceffo <ultimatemal2k@gmail.com>
2025-01-11 18:21:03 +0100

    added fugitive merge remaps

--- lua/kita/plugins/fugitive.lua
+++ lua/kita/plugins/fugitive.lua
@@ -1,3 +1,5 @@
 return {
 	"tpope/vim-fugitive",
+	vim.keymap.set("n", "gh", "<cmd>diffget //2<cr>", { desc = "Get the hunk in the left" }),
+	vim.keymap.set("n", "gl", "<cmd>diffget //3<cr>", { desc = "Get the hunk in the right" }),
 }
