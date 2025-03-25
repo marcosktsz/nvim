d01f5763645f12bf09903e680ea70d310d0122fe
Marco Caceffo <ultimatemal2k@gmail.com>
2025-03-25 21:35:49 +0100

    lspconfig icons

--- lua/kita/plugins/lsp/lspconfig.lua
+++ lua/kita/plugins/lsp/lspconfig.lua
@@ -85,16 +85,7 @@ return {
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
-			vim.diagnostic.config({
-				signs = {
-					text = {
-						error = " ",
-						warn = " ",
-						hint = "󰠠 ",
-						info = " ",
-					},
-				},
-			})
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
 		mason_lspconfig.setup_handlers({
