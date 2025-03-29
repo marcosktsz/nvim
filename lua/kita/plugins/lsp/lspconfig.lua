3d8b13a00ff8738fe90e39e76890384192e056e1
Marco Caceffo <ultimatemal2k@gmail.com>
2025-03-29 12:24:37 +0100

    rusing vim.diagnostic instead of sign_defign

--- lua/kita/plugins/lsp/lspconfig.lua
+++ lua/kita/plugins/lsp/lspconfig.lua
@@ -80,13 +80,22 @@ return {
 		vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { underline = true, undercurl = true, sp = "Blue" })
 		vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { underline = true, undercurl = true, sp = "Green" })
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "󰠠",
+					[vim.diagnostic.severity.HINT] = "",
+				},
+				numhl = {
+					[vim.diagnostic.severity.WARN] = "WarningMsg",
+					[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+					[vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+					[vim.diagnostic.severity.HINT] = "DiagnosticHint",
+				},
+			},
+		})
 
 		mason_lspconfig.setup_handlers({
 			-- default handler for installed servers
