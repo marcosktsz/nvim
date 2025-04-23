8598852cf4b9cb5559d43c11b15e433e26fe4a72
Marco Caceffo <ultimatemal2k@gmail.com>
2025-04-24 00:36:11 +0200

    readded copilot.vim

--- lua/kita/plugins/copilot.lua
+++ lua/kita/plugins/copilot.lua
@@ -1,10 +1,28 @@
 return {
-	"copilotlsp-nvim/copilot-lsp",
-	init = function()
-		vim.g.copilot_nes_debounce = 500
-		vim.lsp.enable("copilot")
-		vim.keymap.set("n", "<C-a>", function()
-			require("copilot-lsp.nes").apply_pending_nes()
-		end)
-	end,
+	{
+		"copilotlsp-nvim/copilot-lsp",
+		init = function()
+			vim.g.copilot_nes_debounce = 500
+			vim.lsp.enable("copilot")
+			vim.keymap.set("n", "<C-a>", function()
+				require("copilot-lsp.nes").apply_pending_nes()
+			end)
+		end,
+	},
+	{
+		"github/copilot.vim",
+		config = function()
+			local function set_copilot_keymap(mode, lhs, rhs)
+				vim.keymap.set(mode, lhs, rhs, {
+					expr = true,
+					replace_keycodes = false,
+					silent = true,
+				})
+			end
+
+			set_copilot_keymap("i", "<C-f>", 'copilot#Accept("<CR>")')
+			set_copilot_keymap("i", "<C-]>", "copilot#Next()")
+			vim.g.copilot_no_tab_map = true
+		end,
+	},
 }
