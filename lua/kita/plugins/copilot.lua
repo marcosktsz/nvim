43ae88596b2eba57acbe07119b62bb5543a44808
Marco Caceffo <ultimatemal2k@gmail.com>
2025-05-07 21:46:31 +0200

    copilot ls updates

--- lua/kita/plugins/copilot.lua
+++ lua/kita/plugins/copilot.lua
@@ -2,10 +2,14 @@ return {
 	{
 		"copilotlsp-nvim/copilot-lsp",
 		init = function()
-			vim.g.copilot_nes_debounce = 0
-			vim.lsp.enable("copilot")
+			vim.g.copilot_nes_debounce = 100
+			vim.lsp.enable("copilot_ls")
 			vim.keymap.set("n", "<C-a>", function()
-				require("copilot-lsp.nes").apply_pending_nes()
+				local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+					or (
+						require("copilot-lsp.nes").apply_pending_nes()
+						and require("copilot-lsp.nes").walk_cursor_end_edit()
+					)
 			end)
 		end,
 	},
