95a9717fcd45a765974a89314511d7dbe5bab964
Marco Caceffo <ultimatemal2k@gmail.com>
2025-05-11 17:36:33 +0200

    removing claude as model due to extreme slowness

--- lua/kita/plugins/ai.lua
+++ lua/kita/plugins/ai.lua
@@ -38,11 +38,6 @@ return {
 	opts = {
 		provider = "copilot",
 		copilot = {
-			endpoint = "https://api.githubcopilot.com",
-			model = "claude-3.5-sonnet",
-			proxy = nil, -- [protocol://]host[:port] Use this proxy
-			allow_insecure = true, -- Allow insecure server connections
-			timeout = 30000, -- Timeout in milliseconds
 			temperature = 0,
 			max_tokens = 50000,
 		},
