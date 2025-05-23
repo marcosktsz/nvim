af181722740e37578be8d8fb92c4c661f530957b
Marco Caceffo <ultimatemal2k@gmail.com>
2025-05-23 19:31:15 +0200

    removed markview from codecompanion to avoid duplicate markdown rendering

--- lua/kita/plugins/ai.lua
+++ lua/kita/plugins/ai.lua
@@ -22,16 +22,6 @@ return {
 				ft = { "markdown", "codecompanion" },
 			},
 			{
-				"OXY2DEV/markview.nvim",
-				lazy = false,
-				opts = {
-					preview = {
-						filetypes = { "markdown", "codecompanion" },
-						ignore_buftypes = {},
-					},
-				},
-			},
-			{
 				"echasnovski/mini.diff",
 				config = function()
 					local diff = require("mini.diff")
